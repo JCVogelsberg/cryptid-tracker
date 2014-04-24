e6f51c99ee2270c2cb2750831f3c5add2048bb69
Patrick McGreevy & Brentwood Reid <student@epicodus.com>
2014-04-24 14:16:55 -0700

    Resolve merge conflicts.
    Remove root to user#index.
    Refactor application layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 CryptidTracker::Application.routes.draw do
   devise_for :users
   resources :users
-<<<<<<< HEAD
-  root to: 'pages#map'
-=======
 
-  root :to => "users#index"
->>>>>>> ae68288f573877d7c5d872d3aa8fb77f3a014212
+  root to: 'pages#map'
+
 end
