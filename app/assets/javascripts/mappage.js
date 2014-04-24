3baef3079097f4a3436cdc60c175674d19dbfa6e
Patrick McGreevy & Brentwood Reid <student@epicodus.com>
2014-04-24 15:19:04 -0700

    write JS function for adding markers to map

--- app/assets/javascripts/mappage.js
+++ app/assets/javascripts/mappage.js
@@ -1,9 +1,23 @@
+var map;
+
+function addMarker(map, lat, lng) {
+  new google.maps.Marker({
+    map: map,
+    draggable: false,
+    position: new google.maps.LatLng(lat, lng)
+  });
+}
+
 function initialize() {
   var mapOptions = {
     center: new google.maps.LatLng(0, 0),
     zoom: 2
   };
-  var map = new google.maps.Map(document.getElementById("map-canvas"),
+  map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
+  addMarker(map,0,0);
 }
+
+
+
 google.maps.event.addDomListener(window, 'load', initialize);
