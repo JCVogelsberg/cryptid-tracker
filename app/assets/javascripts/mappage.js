00a8df91c351200945f86a1511de4a90e8899bbb
Patrick McGreevy & Brentwood Reid <student@epicodus.com>
2014-04-24 15:42:35 -0700

    Place code for adding a marker.

--- app/assets/javascripts/mappage.js
+++ app/assets/javascripts/mappage.js
@@ -1,10 +1,21 @@
 var map;
 
+var image = {
+  url: 'yeti.png',
+  // This marker is 20 pixels wide by 32 pixels tall.
+  size: new google.maps.Size(50, 50),
+  // The origin for this image is 0,0.
+  origin: new google.maps.Point(0,0),
+  // The anchor for this image is the base of the flagpole at 0,32.
+  anchor: new google.maps.Point(0, 32)
+};
+
 function addMarker(map, lat, lng) {
   new google.maps.Marker({
     map: map,
     draggable: false,
     position: new google.maps.LatLng(lat, lng)
+    // icon: image  this is where you put the icon image.
   });
 }
 
