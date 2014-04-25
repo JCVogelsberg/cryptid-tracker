237db9e4dbc9b8803c2e99dd928ce56295ba1564
Brentwood Reid & JCVogelsberg <student@epicodus.com>
2014-04-25 10:15:19 -0700

    Add integration tests for the homepage

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,19 +5,4 @@ describe User do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
-
-
 end
-
-
-
-
-
- # validates :name, presence: true
- #  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
- #  validates :email,
- #            presence: true,
- #            format: { with: VALID_EMAIL_REGEX },
- #            uniqueness: { case_sensitive: false }
- #  validates :password, presence: true, length: { minimum: 6 }
- #  validates :password_digest, presence: true
