e454fba60aa74fca0848f6c7d34c0acec1afabd8
Patrick McGreevy & Paul Ogawa <student@epicodus.com>
2014-04-25 16:48:55 -0700

    user must be signed in to add new sighting and problems resolved

--- spec/factories.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-require 'support/utilities'
-
-FactoryGirl.define do
-  factory :user do
-    email { Faker::Internet.email }
-    password UserAuthentication::DEFAULT_PASSWORD
-  end
-end
