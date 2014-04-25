647ced60e5185181172538ac2709f5095bf27fdb
Brentwood Reid & JCVogelsberg <student@epicodus.com>
2014-04-25 09:27:07 -0700

    Add unit tests for Users and Sightings

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,4 +1,9 @@
 class Sighting <ActiveRecord::Base
   belongs_to :user
 
+  validates :name, :presence => true
+  validates :latitude, :presence => true
+  validates :longitude, :presence => true
+  validates :user_id, :presence => true
+  validates :description, :presence => true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe User do
-  it {should have_many :sightings}
+  it { should have_many :sightings }
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
+
+
+
 end
+
+
+
+
+
+ # validates :name, presence: true
+ #  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+ #  validates :email,
+ #            presence: true,
+ #            format: { with: VALID_EMAIL_REGEX },
+ #            uniqueness: { case_sensitive: false }
+ #  validates :password, presence: true, length: { minimum: 6 }
+ #  validates :password_digest, presence: true
